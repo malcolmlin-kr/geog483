7a1b7f7fcd1085fe55507df7a23e81f99d6f40c7
Jack <mappinjack@users.noreply.github.com>
2018-02-05 13:37:06 -0500

    Add base files for website, starting server

---
+++ dataDict.js
@@ -0,0 +1,8 @@
+
+var dataDict = {
+	"parks" : {
+		"endpoint": "https://services.arcgis.com/ZpeBVw5o1kjit7LT/arcgis/rest/services/ParksSpaces/FeatureServer/0/"},
+	"buildings" : {
+		"endpoint" : "https://services.arcgis.com/ZpeBVw5o1kjit7LT/arcgis/rest/services/Buildings/FeatureServer/0/"}		
+	};
+
